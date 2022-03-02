07315abefb3cc72024d3559709ed6cf18bb935ad
Nick Watt <nwatt@hotmail.com>
2022-03-02 22:25:51 +0000

    Meta charset moved to first element of head

--- wp-content/themes/northeastmetalspinners/header.php
+++ wp-content/themes/northeastmetalspinners/header.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+	<meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta charset="<?php bloginfo('charset'); ?>">
     <?php wp_head(); ?>
 	<script src="https://www.google.com/recaptcha/api.js" async defer></script>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
