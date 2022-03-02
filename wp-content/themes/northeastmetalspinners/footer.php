4ecbfd9c6a08f7a20f9ecc7ccfbd14143386cadb
Nick Watt <nwatt@hotmail.com>
2022-03-02 21:13:29 +0000

    Front page welcome plus admin login to footer

--- wp-content/themes/northeastmetalspinners/footer.php
+++ wp-content/themes/northeastmetalspinners/footer.php
@@ -1,12 +1,51 @@
-    <section class="footer">
-        <table class="color-divider">
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-        </table>
-    </section>
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm">
+                    <div class="news">
+                        <h3 class="heading">Latest <span>News</span></h3>
+                        <?php
+                            $news = new WP_Query( array( 'category_name' => 'news' ) );
+                            $post_counter = 0;
+
+                            if ( $news->have_posts() ) {
+                                while ( $news->have_posts() and $post_counter < 2 ) {
+                                    $news->the_post();?>
+                                    <h4><a href="<?php echo get_post_permalink( $id, $leavename, $sample ); ?>" class="post_title"><?php the_title(); ?></a></h4>
+                                    <p><? echo get_the_excerpt() ?></p>
+                                    <p class="post_date"><?php echo get_the_date( 'F j, Y' ); ?></p>
+                                    <?php
+                                    $post_counter++;
+                                }
+                            }
+                        ?>
+                    </div>
+                </div>
+                <div class="col-sm">
+                    <div class="company-info">
+                        <h3 class="heading">Partscale <span>Ltd.</span></h3>
+                        <p><i class="fa fa-map-marker highlight3"></i>&nbsp;42 Hutton Close, Crowther Industrial Estate, Washington, Tyne & Wear. NE38 0AH</p>
+                        <p><i class="fa fa-phone highlight3"></i>&nbsp;<a href="tel:01914163440" class="highlight3 tel">(0191) 416 3440</a></p>
+                        <p><i class="fa fa-clock highlight3"></i>&nbsp;Mon-Fri: 08:00-17:00</p>
+                    </div>
+                </div>
+                <div class="col-sm">
+                    <h3 class="heading">Get In <span>Touch</span></h3>
+                    <p>Whether you are interested in our products or wanting to provide feedback we would love to hear from you.</p>
+                    <a href="#" class="button highlight1">Send a message</a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm">
+                    <div class="copyright">
+                        <p>&copy; Copyright <?php echo date("Y"); ?> Partscale Ltd., trading as North East Metal Spinners - All rights reserved.</p>
+                        <p>Powered by <a href="https://wordpress.org/" target="_blank">WordPress</a> | Theme by <a href="https://gtctek.co.uk" target="_blank">Gtctek</a> | Admin <a href="/wp-admin">Login</a></p>
+                        <a target="_blank" href="#" class="social-icon color-icon border-icon rounded-icon social-icon soc-facebook"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
     <?php wp_footer(); ?>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
 </body>
--- wp-content/themes/northeastmetalspinners/front-page.php
+++ wp-content/themes/northeastmetalspinners/front-page.php
@@ -1,3 +1,27 @@
 <?php get_header(); ?>
 
+
+        <img src="<?php echo get_template_directory_uri() ?>/img/slide_01.jpg" class="img-fluid" alt="CNC Spinning Machine" />
+
+    <div class="welcome">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h1>Welcome to <span>North East Metal Spinners</span></h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <h7>At North East Metal Spinners, we are specialists in the art and craft of high precision hand metal spinning and in series CNC machine metal spinning.</h7>
+                    <p class="h7afterfix">If you need quality spun products or need advice on whether metal spinning is the answer to your manufacturing needs then you have come to the right company.</p>
+                    <p>Our blend of traditional hand spinning and the latest in CNC machine metal spinning technology puts us in an enviable position to provide our clients with the products they need.</p>
+                    <p>We have a well established track record of providing great service at competitive prices, but without any compromise on quality.</p>
+                    <p>From blueprint to finished product, no shape is too simple or too complex. We can spin metal up to 42″ in diameter with thicknesses ranging from .010″ to .250″ and with tolerances up to .005″.</p>
+                    <p>We can assist you with material selection and the range of finishing options available, as well as providing a range of additional services including welding, punching, beading, anodizing, plating, polishing and custom assembly.</p>
+                </div>
+                <div class="col-sm-6">
+                </div>
+            </div>
+        </div>
+    </div>
 <?php get_footer(); ?>
