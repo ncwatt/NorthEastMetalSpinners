c74457814ae8a3afc9b5d0a5f59f1d786db033fc
Nick Watt <nwatt@hotmail.com>
2022-03-05 01:59:04 +0000

    Temporary text added to page template

--- wp-content/themes/northeastmetalspinners/page.php
+++ wp-content/themes/northeastmetalspinners/page.php
@@ -6,6 +6,7 @@
 				<?php if(has_post_thumbnail()): ?>
 					<img src="<?php the_post_thumbnail_url('post_image'); ?>" class="img-fluid" alt="<?php the_title(); ?>" />
 				<?php endif; ?>
+                page template
 				<h1><?php the_title(); ?></h1>
 				<?php
 					if(have_posts()) : while(have_posts()) : the_post();
