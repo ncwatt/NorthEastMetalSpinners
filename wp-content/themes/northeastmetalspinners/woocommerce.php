24058dca31fdb7ca78fc0387ca7a1272b74dbac5
Nick Watt <nwatt@hotmail.com>
2022-03-10 23:58:12 +0000

    Top and bottom margins added to woocommerce.php

--- wp-content/themes/northeastmetalspinners/woocommerce.php
+++ wp-content/themes/northeastmetalspinners/woocommerce.php
@@ -1,9 +1,8 @@
 <?php get_header(); ?>
 <div class="page-content bg-color1">
-	<div class="container">
+	<div class="container my-3">
 		<div class="row">
 			<div class="col-12">
-                woocomerce template
 				<?php woocommerce_content(); ?>
 			</div>
 		</div>
