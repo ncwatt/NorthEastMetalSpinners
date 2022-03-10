562765b357ddab9c700864160898404ad0b260e1
Nick Watt <nwatt@hotmail.com>
2022-03-10 23:57:53 +0000

    Template-shop renamed to page-shop

--- wp-content/themes/northeastmetalspinners/page-shop.php
+++ wp-content/themes/northeastmetalspinners/page-shop.php
@@ -4,10 +4,9 @@
 */
 get_header(); ?>
 <div class="page-content bg-color1">
-	<div class="container">
+	<div class="container my-3">
 		<div class="row">
 			<div class="col-12">
-				shop template
 				<h1><?php the_title(); ?></h1>
 				<?php
 					if(have_posts()) : while(have_posts()) : the_post();
