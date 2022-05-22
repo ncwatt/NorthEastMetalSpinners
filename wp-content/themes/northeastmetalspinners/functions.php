ea0fc7b0304d91db4e061ce46a3472590b0beba5
Nick Watt <nwatt@hotmail.com>
2022-05-22 22:06:24 +0100

    Check DEV_ENV constant isn't defined or isn't true

--- wp-content/themes/northeastmetalspinners/functions.php
+++ wp-content/themes/northeastmetalspinners/functions.php
@@ -2,10 +2,12 @@
 
 function checkHTTPS()
 {
-    if (! isset($_SERVER['HTTPS']) or $_SERVER['HTTPS'] == 'off' ) {
-        $redirect_url = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-        header("Location: $redirect_url");
-        exit();
+    if ( !defined('DEV_ENV') or constant('DEV_ENV') !== 'true' ) {
+        if (! isset($_SERVER['HTTPS']) or $_SERVER['HTTPS'] == 'off' ) {
+            $redirect_url = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+            header("Location: $redirect_url");
+            exit();
+        }
     }
 }
 
