21f5a8f809580ce4f758dde0301524287350022e
Nick Watt <nwatt@hotmail.com>
2022-07-02 01:42:44 +0100

    Top/bottom margins increased on page

--- wp-content/themes/northeastmetalspinners/page.php
+++ wp-content/themes/northeastmetalspinners/page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <div class="page-content bg-color1">
-	<div class="container my-3">
+	<div class="container my-5">
 		<div class="row">
 			<div class="col-12">
 				<?php if(has_post_thumbnail()): ?>
